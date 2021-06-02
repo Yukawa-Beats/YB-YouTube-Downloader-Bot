20e455f9420df208825a14fd0db374ab238cc9f0
Shamil Habeeb <shamilhabeeb5@gmail.com>
2021-06-02 12:27:55 +0530

    Huh

--- mwk/youtube_callback_data.py
+++ mwk/youtube_callback_data.py
@@ -74,7 +74,7 @@ async def catch_youtube_dldata(c, q):
     if not os.path.isdir(userdir):
         os.makedirs(userdir)
     await q.edit_message_reply_markup(
-        InlineKeyboardMarkup([[InlineKeyboardButton("Downloading.. 🤗.", callback_data="down")]]))
+        InlineKeyboardMarkup([[InlineKeyboardButton("Downloading.... 🤗", callback_data="down")]]))
     filepath = os.path.join(userdir, filext)
     # await q.edit_message_reply_markup([[InlineKeyboardButton("Processing.. 😇")]])
 
@@ -106,7 +106,7 @@ async def catch_youtube_dldata(c, q):
         med = InputMediaAudio(
             media=filename,
             thumb=thumb_image_path,
-            caption=os.path.basename(filename) + "\nUploaded by @mwk_youtubebot",
+            caption=os.path.basename(filename) + "\n\n**Uploaded by @mwk_youtubebot**",
             title=os.path.basename(filename) + "Shamil 9496300461"
         )
 
@@ -119,7 +119,7 @@ async def catch_youtube_dldata(c, q):
             width=width,
             height=height,
             thumb=thumb_image_path,
-            caption=os.path.basename(filename) + "\nUploaded by @mwk_youtubebot",
+            caption=os.path.basename(filename) + "\n\n**Uploaded by @mwk_youtubebot**",
             supports_streaming=True
         )
 
@@ -128,7 +128,7 @@ async def catch_youtube_dldata(c, q):
         med = InputMediaDocument(
             media=filename,
             thumb=thumb_image_path,
-            caption=os.path.basename(filename) + "\nUploaded by @mwk_youtubebot"
+            caption=os.path.basename(filename) + "\n\n**Uploaded by @mwk_youtubebot**"
         )
 
     if cb_data.startswith("docvideo"):
@@ -137,7 +137,7 @@ async def catch_youtube_dldata(c, q):
         med = InputMediaDocument(
             media=filename,
             thumb=thumb_image_path,
-            caption=os.path.basename(filename) + "\nUploaded by @mwk_youtubebot"
+            caption=os.path.basename(filename) + "\n\n**Uploaded by @mwk_youtubebot**"
         )
     if med:
         loop.create_task(send_file(c, q, med, filename))
